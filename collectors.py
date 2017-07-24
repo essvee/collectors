76a6f6cb3313ba7b530f227e162b2e20f50818c4
essvee <fablemouse@gmail.com>
2017-07-24 08:20:12 +0100

    adds alt method for fuzzy matching

--- collectors.py
+++ collectors.py
@@ -1,4 +1,5 @@
 from fuzzywuzzy import process
+import numpy as np
 from fuzzywuzzy import fuzz
 
 
@@ -19,13 +20,34 @@ def cluster_names(input_list):
 
     return clusters
 
+
+def token_set_clustering(input_list):
+    checked = []
+    cluster_list = []
+
+    for name in input_list:
+        cluster = []
+        # Create a new list of names we haven't encountered so far
+        unchecked = np.setdiff1d(input_list, checked, assume_unique=True)
+        for unchecked_name in unchecked:
+            if (fuzz.token_set_ratio(name, unchecked_name) >= 90):
+                cluster.append(unchecked_name)
+                checked.append(unchecked_name)
+            else:
+                cluster.append(name)
+                checked.append(name)
+        # Once we've passed through the whole list, add cluster to cluster_list and return
+        cluster_list.append(cluster)
+        return cluster_list
+
+
 names = ["A Chavez", "A Hamon", "A Harvey", "A Mohyuddin CIBC leg", "A Polaszek", "A Postle", "A wogumi", "A, Mohyuddin CIBC leg.",
          "A. Aguias", "A. Broodbank", "A. Busck", "A. C. Jashi", "A. Garido", "A. Harvey", "A. K. Walker", "A. Lopez-Avila", "A. Michelmore",
          "A. Mohyuddin", "A. Mohyuddin CIBC", "A. Mohyuddin CIBC leg.", "A. Moore", "A. Polac. E", "A. Polac. E.", "A. Polarsek",
          "A. Polaszak", "A. Polaszek", "A. Polaszek et al.", "A. van Harten", "A.Aguiar"]
 
 
-clean_cluster = cluster_names(names)
+clean_cluster = token_set_clustering(names)
 print(names)
 for clean_names in clean_cluster:
     print(clean_names)
