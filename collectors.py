388395657431bfc1b70408ae772674f235647a4c
essvee <fablemouse@gmail.com>
2017-07-24 17:39:11 +0100

    Started IO

--- collectors.py
+++ collectors.py
@@ -1,34 +1,18 @@
-from fuzzywuzzy import process
 import numpy as np
+import pandas as pd
 from fuzzywuzzy import fuzz
 
 cluster_list = []
+names = set()
 
-
-def cluster_names(input_list):
-    clusters = []
-
-    for name in input_list:
-        # Get best matches
-        cluster = process.extractBests(name, input_list, score_cutoff=87)
-
-        # Get just the names - we don't need the match values
-        only_names = [name for name, value in cluster]
-        clusters.append(only_names)
-
-        # Remove touched name from input so we don't get duplicates
-        for remove_name in only_names:
-            names.remove(remove_name)
-
-    return clusters
-
+names = pd.read_csv("coll_names.csv", sep=',', usecols=['Collector1'], squeeze=True)
+print(names)
 
 def token_set_clustering(input_list):
     checked = []
 
     for name in input_list:
         cluster = []
-        # print("Currently looking at: " + name)
 
         # Create a new list of names we haven't encountered so far + turn into a list
         unchecked_list = np.setdiff1d(input_list, checked, assume_unique=True).tolist()
@@ -44,11 +28,11 @@ def token_set_clustering(input_list):
             cluster_list.append(cluster)
 
 
-names = ["A Chavez", "A Hamon", "A Harvey", "A Mohyuddin CIBC leg", "A Polaszek", "A Postle", "A wogumi", "A, Mohyuddin CIBC leg.",
-         "A. Aguias", "A. Broodbank", "A. Busck", "A. C. Jashi", "A. Garido", "A. Harvey", "A. K. Walker", "A. Lopez-Avila", "A. Michelmore",
-         "A. Mohyuddin", "A. Mohyuddin CIBC", "A. Mohyuddin CIBC leg.", "A. Moore", "A. Polac. E", "A. Polac. E.", "A. Polarsek",
-         "A. Polaszak", "A. Polaszek", "A. Polaszek et al.", "A. van Harten", "A.Aguiar"]
+# names = ["A Chavez", "A Hamon", "A Harvey", "A Mohyuddin CIBC leg", "A Polaszek", "A Postle", "A wogumi", "A, Mohyuddin CIBC leg.",
+#          "A. Aguias", "A. Broodbank", "A. Busck", "A. C. Jashi", "A. Garido", "A. Harvey", "A. K. Walker", "A. Lopez-Avila", "A. Michelmore",
+#          "A. Mohyuddin", "A. Mohyuddin CIBC", "A. Mohyuddin CIBC leg.", "A. Moore", "A. Polac. E", "A. Polac. E.", "A. Polarsek",
+#          "A. Polaszak", "A. Polaszek", "A. Polaszek et al.", "A. van Harten", "A.Aguiar"]
 
-token_set_clustering(names)
-for list in cluster_list:
-    print(list)
+# token_set_clustering(names)
+# for name_list in cluster_list:
+#     print(name_list)
